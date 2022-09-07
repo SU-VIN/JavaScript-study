9ab9d26307a1459e95fdd470c0ec64fa0af09901
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2022-09-07 18:41:51 +0900

    ct13

---
+++ 코딩테스트/ct13.js
@@ -0,0 +1,13 @@
+function solution(price, money, count) {
+    let a = 0;
+    for(let i=1;i<=count;i++){
+        a+=price*i;
+    }
+    return a>money?a-money:0;
+
+}
+price =3;
+money =40;
+count =4;
+
+console.log(solution(price,money,count));
