30d50182c095baf58031aa2ae7a3f2e8886b92d0
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2022-09-13 20:15:10 +0900

    ct23

---
+++ 코딩테스트/ct23.js
@@ -0,0 +1,13 @@
+function solution(s){
+    let count =0;
+    for(let i =0;i<s.length;i++){
+        count+=s[i]==="("?1:-1;
+        if(count<0)return false;   //)가 먼저오면 무조건 실패     
+    }
+    return count === 0 ?true:false;
+
+}
+s = "()()";
+s1=")()(";
+
+console.log(solution(s1));
