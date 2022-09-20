7fa324beaec2e5a31d8163e6ebd7d142fb3f8640
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2022-09-20 22:57:41 +0900

    ct30

--- 코딩테스트/ct30.js
+++ 코딩테스트/ct30.js
@@ -1,10 +1,11 @@
-//멀리뛰기
+//피보나치 수
 function solution(n) {
-    var dp = [0,1,2];
-    for(let i=3;i<=n;i++){
+    var dp = [0,1];
+    for(let i=2;i<=n;i++){
         dp[i]=(dp[i-1]+dp[i-2])%1234567;
     }
     return dp[n];
 }
-n=4;
+
+n=3
 console.log(solution(n));
