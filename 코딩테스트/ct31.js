bd5228fdc91dc3eaa69ff5d55a757ecf76bc54ad
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2022-09-21 18:20:54 +0900

    Update ct31.js

--- 코딩테스트/ct31.js
+++ 코딩테스트/ct31.js
@@ -2,11 +2,11 @@
 function solution(brown, yellow) {
     var answer = [];
     var sum = brown+yellow;
-
+    //카펫의 최소길이는 3임
     for(let height=3; height<=brown; height++){
         if(sum%height==0){
             let weight = sum/height;
-
+            //(가로-2)*(높이-2) = 노랑칸 개수 
             if((weight-2)*(height-2)==yellow){
                 return [weight,height]
             }
