7108f3d32e37f76f3e9966f53ddd913904abc324
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2022-09-26 21:31:01 +0900

    ct34

---
+++ 코딩테스트/ct34.js
@@ -0,0 +1,18 @@
+function solution(people, limit) {
+    var count =0;
+    people= people.sort((a,b)=>b-a);
+    
+    for(let i=0, j=people.length-1;i<=j;i++){
+        if(people[i]+people[j]<=limit){
+            j--;
+        }
+       count++;
+    }
+
+    return count;
+}
+
+people = [70,50,80,50];
+limit =100;
+
+console.log(solution(people,limit))
