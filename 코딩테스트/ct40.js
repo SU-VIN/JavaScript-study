ae5afc0dc19d390a62a71eee8ef365a48ef7610d
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2022-10-02 17:56:21 +0900

    ct40

---
+++ 코딩테스트/ct40.js
@@ -0,0 +1,15 @@
+function solution(s)
+{
+    var arr = [];
+    for(let i=0;i<s.length;i+=1){
+       if(arr[arr.length-1]===s[i]){
+           arr.pop()
+       }else{
+           arr.push(s[i])
+       }
+    }
+    return arr.length ===0 ?1:0
+}
+s ="baabaa"
+
+console.log(solution(s));
