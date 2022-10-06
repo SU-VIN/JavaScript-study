dbe59829bead0326b4a6dd04131e342c8e10050c
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2022-10-06 18:50:31 +0900

    ct44

---
+++ 코딩테스트/ct44.js
@@ -0,0 +1,16 @@
+function solution(arr, divisor) {
+    var answer = [];
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]%divisor==0){
+            answer.push(arr[i])
+        }
+    }
+    if(answer.length<1){
+        answer.push(-1)
+    }
+    return answer.sort((a,b)=>a-b);
+}
+
+arr = [2,3,4]
+divisor = 5
+console.log(solution(arr,divisor))
