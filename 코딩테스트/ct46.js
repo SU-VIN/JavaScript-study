bb0203f14d182e8af5a5b9ca21fc4b34c8d34602
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2022-10-07 23:21:04 +0900

    ct46

---
+++ 코딩테스트/ct46.js
@@ -0,0 +1,14 @@
+//수박수박수박수박수박수?
+function solution(n) {
+    var answer =""
+    for(let i=1;i<=n;i++){
+        if(i%2!=0){
+            answer+="수"
+        }else{
+            answer+="박"
+        }
+    }
+    return answer
+}
+n=3
+console.log(solution(n))
