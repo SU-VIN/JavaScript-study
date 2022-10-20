e68bed2339a2504db34bdfe7f2254fc92b41f24b
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2022-10-20 20:19:01 +0900

    Update 4.js

--- 코딩테스트/inflearn/section2/4.js
+++ 코딩테스트/inflearn/section2/4.js
@@ -1,6 +1,15 @@
 //점수 계산
-function solution(arr){         
-
+function solution(arr){     
+    let sum=0; 
+    let score=1;   
+    for(let i =0;i<arr.length;i++){
+        if(arr[i]==0){
+            score =0;
+        }
+        sum+=score;
+        score++;
+    }
+    return sum;
 }
 
 let arr=[1, 0, 1, 1, 1, 0, 0, 1, 1, 0];
