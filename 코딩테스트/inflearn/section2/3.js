69ca5fea0820364b2dadcaee261d4b514402e444
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2022-10-20 20:13:17 +0900

    Update 3.js

--- 코딩테스트/inflearn/section2/3.js
+++ 코딩테스트/inflearn/section2/3.js
@@ -1,5 +1,23 @@
 //가위바위보
 function solution(a, b){         
+//가위 1 바위 2 보 3
+    for(let i =0;i<a.length;i++){
+        if(a[i]==1&&b[i]==3){
+            console.log("A")
+        }
+        else if(a[i]==2&&b[i]==1){
+            console.log("A")
+        }
+        else if(a[i]==3&&b[i]==2){
+            console.log("A")
+        }
+        else if(a[i]===b[i]){
+            console.log("D")
+        }
+        else{
+            console.log("B")
+        }
+    }
 
 }
 
