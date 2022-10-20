073f71d2aeda54297943aa9b48b97d4593f31cba
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2022-10-20 20:03:00 +0900

    Update 2.js

--- 코딩테스트/inflearn/section2/2.js
+++ 코딩테스트/inflearn/section2/2.js
@@ -1,6 +1,12 @@
 //보이는 학생
-function solution(arr){         
-
+function solution(arr){    
+    let count = 0;     
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]<arr[i+1]){
+            count++
+        }
+    }
+    return count
 }
 
 let arr=[130, 135, 148, 140, 145, 150, 150, 153];
