a2b412661fbe06b706430b570a51a2c5ef4e40f7
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2022-10-27 14:05:09 +0900

    Update 6.js

--- 코딩테스트/inflearn/section2/6.js
+++ 코딩테스트/inflearn/section2/6.js
@@ -1,6 +1,25 @@
 //격자판 최대합
 function solution(arr){  
+    let answer=0;
+    let res1=0;
+    let res2=0;
+    let res3=0;
+    let res4=0;
+    for(let i=0;i<arr.length;i++){
+        res1=res2=0
+        for(let j=0;j<arr.length;j++){
+            res1+=arr[i][j]
+            res2+=arr[j][i]
+        }
+        answer = Math.max(res1,res2)
+    }
 
+    for(let i=0;i<arr.length;i++){
+        res3+=arr[i][i]
+        res4+=arr[i][arr.length-i-1]
+    }
+    answer = Math.max(answer,res3,res4)
+    return answer
 }
 
 let arr=[[10, 13, 10, 12, 15], 
