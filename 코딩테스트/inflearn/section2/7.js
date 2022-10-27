019f21d586672928af3de34f0a6bc061ce889c8b
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2022-10-27 17:17:43 +0900

    Update 7.js

--- 코딩테스트/inflearn/section2/7.js
+++ 코딩테스트/inflearn/section2/7.js
@@ -1,5 +1,24 @@
 //봉우리
 function solution(arr){  
+    let count =0;
+    //12 3 6 9 방향
+    let dx =[-1,0,1,0]
+    let dy =[0,1,0,-1]
+    for(let i=0;i<arr.length;i++){
+        for(let j=0;j<arr.length;j++){
+            let flag= 1
+            for(let k=0;k<dx.length;k++){
+                let nx=i+dx[k]
+                let ny=j+dy[k]
+                if(nx>=0&&nx<arr.length&&ny>=0&&ny<arr.length&&arr[nx][ny]>=arr[i][j]){
+                    flag=0
+                    break
+                }
+            }
+            if(flag) count++
+        }
+    }
+    return count
 }
 
 let arr=[[5, 3, 7, 2, 3], 
