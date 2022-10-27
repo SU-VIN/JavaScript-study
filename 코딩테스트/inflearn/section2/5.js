fdec922940e1fff7698b3065fb9e44a627dd48d3
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2022-10-27 13:47:53 +0900

    Update 5.js

--- 코딩테스트/inflearn/section2/5.js
+++ 코딩테스트/inflearn/section2/5.js
@@ -1,7 +1,15 @@
 //등수구하기
 function solution(arr){  
-
+    let result = [1,1,1,1,1]
+    for(let i=0;i<arr.length;i++){
+        for(let j =0;j<arr.length;j++){
+            if(arr[i]<arr[j]){
+                result[i]++
+            }
+        }
+    }
+    return result
 }
 
-let arr=[87, 89, 92, 100, 76];
+let arr=[87, 87, 92, 100, 76];
 console.log(solution(arr));
