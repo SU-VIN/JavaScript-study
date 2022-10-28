9c7ad8bc2b322583ff52cdebe4c9194d5072c036
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2022-10-28 23:26:20 +0900

    Update 1.js

--- 코딩테스트/inflearn/section3/1.js
+++ 코딩테스트/inflearn/section3/1.js
@@ -1,9 +1,19 @@
 //회문문자열
 function solution(s){
+    let count =0;
+        for(let i=0;i<Math.floor(s.length/2);i++){
+            console.log(s[i].toUpperCase())
+            console.log(s[s.length-i-1].toUpperCase())
+            if(s[i].toUpperCase()==s[s.length-i-1].toUpperCase()){
+                count++
+            }
+        }
+
+    return count==Math.floor(s.length/2)?"YES":"NO"
 
 }
 
-let str="goooG";
+let str="gooG";
 console.log(solution(str));
 
 
