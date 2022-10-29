0d5bdc61d2662947efb25c14d373ae5bc814a4e8
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2022-10-29 19:58:14 +0900

    Update 4.js

--- 코딩테스트/inflearn/section3/4.js
+++ 코딩테스트/inflearn/section3/4.js
@@ -1,7 +1,30 @@
 //가장 짧은 문자거리
 function solution(s, t){
+    let start = 1000;
+    let arr=[];
+    for(let i=0;i<s.length;i++){
+        if(s[i]===t){
+            start=0;
+            arr.push(start)
+        }else{
+            arr.push(start);
+        }
+        start++
+    }
 
+    for(let i=s.length-1;i>=0;i--){
+        if(s[i]===t){
+            start=0;
+        }else{
+            if(start<arr[i]) arr[i]=start
+        }
+        start++
+        
+    }
+
+    return arr;
 }
 
 let str="teachermode";
 console.log(solution(str, 'e'));
+
