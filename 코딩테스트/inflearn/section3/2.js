d3d0bbd30c00260dd593eb6a755e4f0be3bc1dbc
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2022-10-29 18:48:03 +0900

    Update 2.js

--- 코딩테스트/inflearn/section3/2.js
+++ 코딩테스트/inflearn/section3/2.js
@@ -1,6 +1,8 @@
 //유효한 팰린드룸
 function solution(s){
-
+    //정규식 사용 (/[^a-z]/g,"") 글로벌 영역에서 소문자가 아닌걸 찾아서 빈문자열로 바꿔라 (^가 부정을 뜻함)
+    s=s.toLowerCase().replace(/[^a-z]/g,"")
+    return s.split('').reverse().join('')===s?"YES":"NO"
 }
 
 let str="found7, time: study; Yduts; emit, 7Dnuof";
