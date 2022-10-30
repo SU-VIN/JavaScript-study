c28a88bc816a65e229d30ad1f238e64401282709
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2022-10-30 23:11:43 +0900

    Update 5.js

--- 코딩테스트/inflearn/section3/5.js
+++ 코딩테스트/inflearn/section3/5.js
@@ -1,6 +1,18 @@
 //문자열 압축
 function solution(s){
+    let result = ""
+    let count=1;
+    for(let i=0;i<s.length;i++){
+        if(s[i]===s[i+1]){
+            count++;
+        }else{
+            result+=s[i]
+            if(count!==1) result+=count;
+            count=1;
+        }
 
+    }
+    return result;
 }
 
 let str="KKHSSSSSSSE";
