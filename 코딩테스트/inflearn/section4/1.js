b7bf8a3112d2c076c46514201ddeeb48ef3a4ec7
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2022-11-05 17:31:42 +0900

    Update 1.js

--- 코딩테스트/inflearn/section4/1.js
+++ 코딩테스트/inflearn/section4/1.js
@@ -1,6 +1,27 @@
 //자리수의 합
 function solution(n, arr){
+    let sum=0;
+    let result=[];
+    let max =0;
+    let answer;
+    const newArr= arr.map((v)=>{return String(v)})
 
+    for(let i=0;i<newArr.length;i++){
+        for(let j=0; j<newArr[i].length;j++){
+            sum+=Number(newArr[i][j]);
+            
+        }
+        if(sum>max){
+            max=sum;
+            answer = newArr[i]
+        }
+        else if(sum===max){
+            if(newArr[i]>answer)answer=newArr[i]
+        }
+        sum=0;
+    }
+
+    return answer;
 }
 
 let arr=[128, 460, 603, 40, 521, 137, 123];
