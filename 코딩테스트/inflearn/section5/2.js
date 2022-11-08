bf3f0ee31d4ce6e823ee6ad5e6a750b9bb30282f
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2022-11-08 22:10:13 +0900

    Update 2.js

--- 코딩테스트/inflearn/section5/2.js
+++ 코딩테스트/inflearn/section5/2.js
@@ -1,5 +1,15 @@
 //공통원소구하기
 function solution(arr1, arr2){
+    let result = [];
+    for(let i=0;i<arr1.length;i++){
+        for(let j=0; j<arr2.length;j++){
+            if(arr1[i]==arr2[j]){
+                result.push(arr2[j])
+            }
+        }
+    }
+    result.sort((a,b)=>a-b)
+    return result
 
 }
 
